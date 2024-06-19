ef506b36ae62b233224df404de339ba9c25e3775
Kacper  Jagodziński <kacperjagodzinski@op.pl>
2024-06-19 23:28:22 +0200

    making tests

--- working_app/tests/test_selenium.py
+++ working_app/tests/test_selenium.py
@@ -1,9 +1,13 @@
+from telnetlib import EC
+
 from django.test import LiveServerTestCase
 from djoser.conf import User
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 from product.models import Category, Product
 from register.models import UserProfile
@@ -43,7 +47,7 @@ class SeleniumTests(LiveServerTestCase):
         # Utworzenie produktów testowych
         self.product1 = Product.objects.create(
             category=self.category,
-            name='Test Product 1',
+            name='Test Product 1 Bluza',
             slug='test-product-1',
             description='Description for test product 1',
             price='9.99',
@@ -53,12 +57,13 @@ class SeleniumTests(LiveServerTestCase):
         )
         self.product2 = Product.objects.create(
             category=self.category,
-            name='Test Product 2',
+            name='Test Product 2 bluza',
             slug='test-product-2',
             description='Description for test product 2',
             price='19.99',
             image='images/test-image2.jpg',  # Tutaj podaj ścieżkę do obrazu, który chcesz przetestować
-            thumbnail='images/test-thumbnail2.jpg',  # Tutaj podaj ścieżkę do miniatury obrazu, który chcesz przetestować
+            thumbnail='images/test-thumbnail2.jpg',
+            # Tutaj podaj ścieżkę do miniatury obrazu, który chcesz przetestować
             date_added='2022-01-01 12:00:00'
         )
         self.product3 = Product.objects.create(
@@ -68,7 +73,8 @@ class SeleniumTests(LiveServerTestCase):
             description='Description for test product 3',
             price='29.99',
             image='images/test-image3.jpg',  # Tutaj podaj ścieżkę do obrazu, który chcesz przetestować
-            thumbnail='images/test-thumbnail3.jpg',  # Tutaj podaj ścieżkę do miniatury obrazu, który chcesz przetestować
+            thumbnail='images/test-thumbnail3.jpg',
+            # Tutaj podaj ścieżkę do miniatury obrazu, który chcesz przetestować
             date_added='2022-01-01 12:00:00'
         )
 
@@ -277,4 +283,188 @@ class SeleniumTests(LiveServerTestCase):
 
         # Sprawdzenie czy strona produktu się załadowała
         assert 'Test Product 1' in self.selenium.page_source
-        assert 'Description for test product 1' in self.selenium.page_source # Jest tylko na stronie konkretnego produktu
+        assert 'Description for test product 1' in self.selenium.page_source  # Jest tylko na stronie konkretnego produktu
+
+    ####################### Filters ########################
+    def test_filter_name(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        search_input = self.selenium.find_element(By.XPATH, '//*[@id="search"]')
+
+        # Wpisanie tekstu "bluza" do pola wyszukiwania
+        search_input.send_keys('bluza')
+        search_input.send_keys(Keys.ENTER)
+
+        # Czekamy na załadowanie wyników
+        self.selenium.implicitly_wait(5)
+        # time.sleep(20)
+
+        # Sprawdzamy, czy odpowiednie produkty są wyświetlane na stronie
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+        assert 'Test Product 2 bluza' in self.selenium.page_source
+
+        # Dodatkowo, możemy sprawdzić, czy na stronie nie ma produktów bez słowa "bluza" w nazwie
+        assert 'Test Product 3' not in self.selenium.page_source
+
+    def test_filter_price(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        # Maybe later I will repair this test
+        # # Odnajdujemy pola cenowe
+        # min_price_input = self.selenium.find_element(By.XPATH, '//*[@id="price_from"]')
+        # max_price_input = self.selenium.find_element(By.XPATH, '//*[@id="price_to"]')
+        #
+        # # Wprowadzamy minimalną cenę
+        # min_price_input.send_keys('5')
+        #
+        # # Czekamy krótko, aby przeglądarka przetworzyła wprowadzenie danych
+        # WebDriverWait(self.selenium, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="price_to"]')))
+        #
+        # # Odnajdujemy pole maksymalnej ceny ponownie
+        # max_price_input = self.selenium.find_element(By.XPATH, '//*[@id="price_to"]')
+        #
+        # # Wprowadzamy maksymalną cenę
+        # max_price_input.send_keys('15')
+        # max_price_input.send_keys(Keys.ENTER)
+        #
+        # # Czekamy na załadowanie wyników
+        # WebDriverWait(self.selenium, 10).until(
+        #     EC.presence_of_element_located((By.XPATH, '//*[@id="filtered_products"]')))  # XPath do kontenera wyników
+        #
+        # # Sprawdzamy, czy odpowiednie produkty są wyświetlane na stronie
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+
+    def test_filter_category(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        # Maybe later I will repair this test
+        # # Znalezienie checkboxa dla kategorii "Bluzy"
+        # bluzy_checkbox = self.selenium.find_element(By.XPATH, '//*[@id="categories"]/li[1]/input')
+        #
+        # # Kliknięcie checkboxa, aby go zaznaczyć
+        # bluzy_checkbox.click()
+        #
+        # # Czekamy na załadowanie wyników (możesz zwiększyć czas oczekiwania w zależności od czasu potrzebnego na załadowanie)
+        # self.selenium.implicitly_wait(5)
+        #
+        # # Sprawdzamy, czy odpowiednie produkty są wyświetlane na stronie
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+        assert 'Test Product 2 bluza' in self.selenium.page_source
+        assert 'Test Product 3' in self.selenium.page_source
+
+    ####################### Cart ########################
+    def test_shopping_cart_view(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element(By.NAME, "username")
+        password_input = self.selenium.find_element(By.NAME, "password")
+        username_input.send_keys('test1')
+        password_input.send_keys('haslo12345')
+        login_button = self.selenium.find_element(By.XPATH, "//button[@type='submit' and contains(text(), 'Zaloguj')]")
+        login_button.click()
+        time.sleep(5)
+        assert 'Wyloguj się' in self.selenium.page_source
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        # Znalezienie i kliknięcie linku do produktu
+        try:
+            product_link = self.selenium.find_element(By.XPATH, "//a[@href='/product/1' and @class='card w-100 h-100']")
+            product_link.click()
+            time.sleep(2)  # Poczekaj na załadowanie nowej strony
+        except Exception as e:
+            self.fail(f"Product link not found or could not be clicked: {e}")
+
+        # Sprawdzenie czy strona produktu się załadowała
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+        assert 'Description for test product 1' in self.selenium.page_source  # Jest tylko na stronie konkretnego produktu
+
+        add_to_cart_button = self.selenium.find_element(By.XPATH,
+                                                        "//button[@type='submit' and contains(text(), 'Dodaj do koszyka')]")
+        add_to_cart_button.click()
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/cart/'))
+        time.sleep(5)
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+
+    def test_shopping_cart_duplicate_product(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element(By.NAME, "username")
+        password_input = self.selenium.find_element(By.NAME, "password")
+        username_input.send_keys('test1')
+        password_input.send_keys('haslo12345')
+        login_button = self.selenium.find_element(By.XPATH, "//button[@type='submit' and contains(text(), 'Zaloguj')]")
+        login_button.click()
+        time.sleep(5)
+        assert 'Wyloguj się' in self.selenium.page_source
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        # Znalezienie i kliknięcie linku do produktu
+        try:
+            product_link = self.selenium.find_element(By.XPATH, "//a[@href='/product/1' and @class='card w-100 h-100']")
+            product_link.click()
+            time.sleep(2)  # Poczekaj na załadowanie nowej strony
+        except Exception as e:
+            self.fail(f"Product link not found or could not be clicked: {e}")
+
+        # Sprawdzenie czy strona produktu się załadowała
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+        assert 'Description for test product 1' in self.selenium.page_source  # Jest tylko na stronie konkretnego produktu
+
+        add_to_cart_button = self.selenium.find_element(By.XPATH,
+                                                        "//button[@type='submit' and contains(text(), 'Dodaj do koszyka')]")
+        add_to_cart_button.click()
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/cart/'))
+        time.sleep(5)
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+
+        product_quantity = self.selenium.find_element(By.XPATH, "//input[@type='number' and @value='1']")
+        product_quantity.clear()
+        product_quantity.send_keys('2')
+        assert product_quantity.get_attribute('value') == '2'
+
+    def test_shopping_cart_delete_product(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element(By.NAME, "username")
+        password_input = self.selenium.find_element(By.NAME, "password")
+        username_input.send_keys('test1')
+        password_input.send_keys('haslo12345')
+        login_button = self.selenium.find_element(By.XPATH, "//button[@type='submit' and contains(text(), 'Zaloguj')]")
+        login_button.click()
+        time.sleep(5)
+        assert 'Wyloguj się' in self.selenium.page_source
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        # Znalezienie i kliknięcie linku do produktu
+        try:
+            product_link = self.selenium.find_element(By.XPATH, "//a[@href='/product/1' and @class='card w-100 h-100']")
+            product_link.click()
+            time.sleep(2)  # Poczekaj na załadowanie nowej strony
+        except Exception as e:
+            self.fail(f"Product link not found or could not be clicked: {e}")
+
+        # Sprawdzenie czy strona produktu się załadowała
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+        assert 'Description for test product 1' in self.selenium.page_source  # Jest tylko na stronie konkretnego produktu
+
+        add_to_cart_button = self.selenium.find_element(By.XPATH,
+                                                        "//button[@type='submit' and contains(text(), 'Dodaj do koszyka')]")
+        add_to_cart_button.click()
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/cart/'))
+        time.sleep(5)
+        assert 'Test Product 1 Bluza' in self.selenium.page_source
+
+        # TODO nie może znaleść tego <a> i wywala błąd
+        # # Kliknięcie w przycisk 'Usuń' dla produktu w koszyku
+        # try:
+        #     delete_button = self.selenium.find_element(By.XPATH,
+        #                                           "//a[@data-product='1']")
+        #     delete_button.click()
+        #     time.sleep(2)  # Poczekaj na wykonanie akcji usunięcia
+        # except Exception as e:
+        #     self.fail(f"Delete button not found or could not be clicked: {e}")
+        #
+        # # Odświeżenie strony koszyka
+        # self.selenium.get('%s%s' % (self.live_server_url, '/cart/'))
+        # time.sleep(5)  # Poczekaj na załadowanie się strony koszyka po usunięciu produktu
+        #
+        # # Sprawdzenie, czy produkt został usunięty
+        # assert 'Test Product 1 Bluza' not in self.selenium.page_source
+        # assert 'Twój koszyk jest pusty' in self.selenium.page_source
